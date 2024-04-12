ddf2605729a4beb2975154f18e9f02f0c506a9ef
Gerardo Martín <gerardo@pop-os.localdomain>
2024-04-12 13:25:25 -0600

    Sorting index valid

---
+++ Tutorial-SDM/Sorting-index.R
@@ -0,0 +1,12 @@
+library(terra)
+
+v <-  vect("Tutorial-SDM/Cuadricula-brad.gpkg")
+
+byn <- rep(c(1, 0), 14)
+byn1 <-  rep(c(0, 1), 14)
+
+byn2 <- rep(c(byn, byn1), 15)
+
+v$ByN <- byn2
+
+writeVector(v, "Tutorial-SDM/Cuadricula-brad.gpkg", overwrite = T)
